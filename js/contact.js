672ca221094135bdaa586e8062a981c9569319cd
victorscher <victorscher92@gmail.com>
2020-09-02 19:42:13 -0300

    feat: almost finish

--- js/contact.js
+++ js/contact.js
@@ -3,12 +3,27 @@ const whatsBtn = document.getElementById('whatsapp');
 
 select.addEventListener('change', (e) => {
   if (select.value != '') {
-    whatsBtn.href = `https://api.whatsapp.com/send?phone=${select.value}&text=Ol%C3%A1%2C%20estava%20olhando%20o%20site%20de%20voc%C3%AAs%20e%20gostaria%20de%20obter%20um%20or%C3%A7amento`;
+    whatsBtn.href = `https://api.whatsapp.com/send?phone=${select.value}`;
     whatsBtn.style.color = '#da7d21';
     whatsBtn.style.textDecoration = 'none';
     whatsBtn.style.pointerEvents = 'initial';
   } else {
     whatsBtn.style.color = 'gray';
     whatsBtn.style.pointerEvents = 'none';
+  }
+});
+
+const select2 = document.getElementById('city2');
+const whatsBtn2 = document.getElementById('whatsapp2');
+
+select2.addEventListener('change', (e) => {
+  if (select2.value != '') {
+    whatsBtn2.href = `https://api.whatsapp.com/send?phone=${select2.value}`;
+    whatsBtn2.style.color = '#31709e';
+    whatsBtn2.style.textDecoration = 'none';
+    whatsBtn2.style.pointerEvents = 'initial';
+  } else {
+    whatsBtn2.style.color = 'gray';
+    whatsBtn2.style.pointerEvents = 'none';
   }
 });
