69289e2021e4c66a677d8ed998d9377a20471156
victorscher <victorscher92@gmail.com>
2020-09-08 16:07:25 -0300

    fix: map positions

--- js/contact.js
+++ js/contact.js
@@ -17,6 +17,26 @@ select.addEventListener('change', (e) => {
     localizationBtn.style.color = 'gray';
     localizationBtn.style.pointerEvents = 'none';
   }
+
+  if (select.value == '557734512150') {
+    city = 'gbi';
+    logradouro.textContent = gbi.logradouro;
+    bairro.textContent = gbi.bairro;
+    cep.textContent = gbi.cep;
+    initMap();
+  } else if (select.value == '5577999760046') {
+    city = 'bar';
+    logradouro.textContent = bar.logradouro;
+    bairro.textContent = bar.bairro;
+    cep.textContent = bar.cep;
+    initMap();
+  } else if (select.value == '5577999610770') {
+    city = 'vca';
+    logradouro.textContent = vca.logradouro;
+    bairro.textContent = vca.bairro;
+    cep.textContent = vca.cep;
+    initMap();
+  }
 });
 
 const select2 = document.getElementById('city2');
@@ -36,13 +56,71 @@ select2.addEventListener('change', (e) => {
 
 const modal = document.querySelector('.map-modal');
 const close = modal.querySelector('.close');
-const gbi = { lat: -14.2097524, lgn: -42.7694645 };
-const bar = {lat: -12.1475489, lng: -44.9952351}
+
+const logradouro = document.querySelector('.logradouro');
+const bairro = document.querySelector('.bairro');
+const cep = document.querySelector('.cep');
+
+const gbi = {
+  logradouro: 'Av. Barão do Rio Branco, N° 1972,',
+  bairro: 'São Fransisco, Guanambi, Bahia,',
+  cep: '46430-000',
+};
+
+const bar = {
+  logradouro: 'Rua Coronel Magno, N° 636,',
+  bairro: 'Centro, Barreiras, Bahia',
+  cep: '47800-166',
+};
+
+const vca = {
+  logradouro: 'Av. Juracy Magalhães, N° 389,',
+  bairro: 'Felicia, Vitória da Conquista, Bahia,',
+  cep: '45023-490',
+};
 
 localizationBtn.addEventListener('click', () => {
   modal.style.transform = 'scale(1)';
+  modal.style.opacity = '1';
 });
 
 close.addEventListener('click', () => {
   modal.style.transform = 'scale(0)';
+  modal.style.opacity = '0';
 });
+
+modal.addEventListener('transitionend', (e) => {
+  if (e.propertyName == 'transform') {
+    initMap();
+  }
+});
+
+let city = '';
+
+function initMap() {
+  // The locations
+  let gbi = { lat: -14.2097524, lng: -42.7694645 };
+  let bar = { lat: -12.1475489, lng: -44.9952351 };
+  let vca = { lat: -14.8675967, lng: -40.8444244 };
+
+  // The map
+  if (city == 'gbi') {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: gbi,
+    });
+    var marker = new google.maps.Marker({ position: gbi, map: map });
+  } else if (city == 'bar') {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: bar,
+    });
+    var marker = new google.maps.Marker({ position: bar, map: map });
+  } else if (city == 'vca') {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: vca,
+    });
+    var marker = new google.maps.Marker({ position: vca, map: map });
+  }
+}
